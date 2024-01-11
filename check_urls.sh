857a5a90ed634a4c451991011346ae2cf6458605
Alexandr Savca <alexandr.savca89@gmail.com>
2024-01-11 17:50:38 +0200

    check_urls.sh: check on newsboat.urls

--- check_urls.sh
+++ check_urls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Check urls.opml for dead links.
 
-grep -Eiho "https?://[^\"\\'> ]+" urls.opml | xargs -r -P10 -I{} \
+grep -Eiho "https?://[^\"\\'> ]+" newsboat.urls | xargs -r -P10 -I{} \
 	curl -I -o /dev/null -sw "[%{http_code}] %{url}\n" '{}'  |
 	grep -v '^\[200\]' | sort -u
 
